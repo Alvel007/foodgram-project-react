d0d9b5a89344606be16f17c7e76331a38dfb95f9
Алексей Кирилишин <alexkak@inbox.ru>
2023-10-18 02:32:48 +0400

    rev_4

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -126,4 +126,3 @@ class RecipeViewSet(viewsets.ModelViewSet):
         response = HttpResponse('Cписок покупок:\n' + '\n'.join(shopping_list),
                                 content_type='text/plain')
         return response
-
