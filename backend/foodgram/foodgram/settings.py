45981643bcaa00f3e0f359a6532af90a50aa3282
Алексей Кирилишин <alexkak@inbox.ru>
2023-10-19 00:44:02 +0400

    commen-26

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -63,11 +63,11 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgre_db_prod',
-        'USER': 'foodgram',
-        'PASSWORD': 'foodgram',
-        'HOST': 'postgre_db_prod',
-        'PORT': 5432,
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
