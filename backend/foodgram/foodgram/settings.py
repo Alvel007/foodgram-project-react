dfd8ebcf1fe7a484ce7da6af288df4dfe3091e98
Алексей Кирилишин <alexkak@inbox.ru>
2023-10-18 23:40:00 +0400

    commen-21

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'HOST': 'postgre_db_prod',
         'PORT': os.getenv('DB_PORT', 5432),
     }
 }
